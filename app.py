31f4d74d409744196dac6c1461223a273d4f0593
bruna totti <totti_bruna@hotmail.com>
2020-06-07 09:30:19 -0300

    Vinculos entre as páginas

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from flask.templating import render_template
 
-app = Flask(__name__)
+app = Flask (__name__)
 
-@app.route("/", methods=["GET"])
-def home():
-    return "Olá, Mundo!"
+@app.route("/", methods = ['GET'])
+@app.route ("/login", methods = ['GET'])
+@app.route ("/bruna", methods = ['GET'])
+def login ():
+     return render_template('login.html')
 
-app.run()
+@app.route("/cadastro", methods = ['GET'])
+def cadastro ():
+    return render_template('cadastro.html')
+
+@app.route("/index", methods = ['GET'])
+def index ():
+    return render_template('index.html')
+
+@app.route("/historico", methods = ['GET'])
+def historico ():
+    return render_template('historico.html')
